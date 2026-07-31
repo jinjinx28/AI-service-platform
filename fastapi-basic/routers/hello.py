67276ddb0a51844546f9e24b4ade3f93e4176718
jinjin <jinjinx28@gmail.com>
2026-07-31 13:52:02 +0900

    20026-07-31 commit

--- fastapi-basic/routers/hello.py
+++ fastapi-basic/routers/hello.py
@@ -1,6 +1,14 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 hello_router = APIRouter()
+
+person_list = []
+
+ # Person 클래스 정의
+class Person(BaseModel) :
+    name : str
+    age : int
 
 @hello_router.get("/hello")
 async def say_hello() -> dict :     # (key : value ..)
@@ -9,7 +17,8 @@ async def say_hello() -> dict :     # (key : value ..)
     }
 
 @hello_router.post("/hello")
-async def say_hello() -> dict :     # (key : value ..)
+async def say_hello(person: Person) -> dict :     
+    person_list.append(person)
     return {
-        "message" : "hello nuri"
+        "message" : person
     }
