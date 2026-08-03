1ff223b771eabccc02edb72718e3ef900a19e9b2
jinjin <jinjinx28@gmail.com>
2026-08-03 16:49:40 +0900

    2026-08-03 commit

--- fastapi-basic/ch03/main.py
+++ fastapi-basic/ch03/main.py
@@ -4,10 +4,10 @@ from routers.todo import todo_router
 app = FastAPI()
 
 @app.get("/")
-async def welcome() -> dict :
+async def welcome() -> dict:
     return {
-        "message" : "welcome ch03 !!"
+        "message": "welcome ch03!!"
     }
 
 # todo 애플리케이션 개발 - CRUD
-app.include_roouter(todo_router)
+app.include_router(todo_router)
--- fastapi-basic/ch03/routers/todo.py
+++ fastapi-basic/ch03/routers/todo.py
@@ -1,78 +1,81 @@
 from fastapi import APIRouter, Path
-from schemas.todo_schema import Todo, TodoItem, TodoItems
+from pydantic import BaseModel
 
 todo_router = APIRouter()
+
+# Todo Model
+class Todo(BaseModel):
+    id: int
+    item: str
 
 # todo_list
 todo_list = []
 
 # C
-@todo_router("/todo")
-async def add_todo(todo : Todo) -> dict : 
-    todo_list.append(Todo)
+@todo_router.post("/todo")
+async def add_todo(todo: Todo) -> dict:
+    todo_list.append(todo)
     return {
-        "message" : "Todo 객체 추가 완료"
+        "message": "Todo 객체 추가 완료!!"
     }
 
-# R 
+# R
+# all
 @todo_router.get("/todo")
-async def getAll() -> dict :
+async def getAll() -> dict:
     return {
-        "message" : todo_list
+        "message": todo_list
     }
 
 # id
 @todo_router.get("/todo/{id}")
-async def getId() -> dict :
-    for todo in todo_list :
-        if todo.id == id :
+async def getId(id: int) -> dict:
+    for todo in todo_list:
+        if todo.id == id:
             return {
-                "message" : todo
+                "message": todo
             }
-        
     return {
-        "message" : "id가 존재하지 않습니다."
+        "message": "id가 존재하지 않음!!"
     }
 
-# U 
+
+# U
 @todo_router.put("/todo/{id}")
-async def update_todo(todo_data : TodoItem, id : int = Path(...)) -> dict :
-    for todo in todo_list :
-        if todo.id == id :
+async def update_todo(todo_data: Todo, id: int = Path(...)) -> dict:
+    for todo in todo_list:
+        if todo.id == id:
             todo.item = todo_data.item
             return {
-                "message" : "todo 업데이트 성공"
+                "message": "todo 업데이트 성공!!"
             }
-        
     return {
-        "message" : "id가 존재하지 않습니다."
+        "message": "id가 존재하지 않음!!"
     }
 
 # D
-
 # all
-@todo_router.delete("/todo", response_model = TodoItems)
-async def deleteAll() -> dict :
-    if len(todo_list) > 0 :
+@todo_router.delete("/todo")
+async def deleteAll() -> dict:
+    if len(todo_list) > 0:
         todo_list.clear()
         return {
-            "message" : "todo 삭제 성공"
+            "message": "todo_list 삭제 성공!!"
         }
     return {
-        "message" : "todo_list가 존재하지 않습니다"
+        "message": "todo_list 데이터가 존재하지 않음"
     }
 
 # id
 @todo_router.delete("/todo/{id}")
-async def deleteId(id : int) -> dict :
-    for index in range(len(todo_list)) :
+async def deleteId(id: int) -> dict:
+    for index in range(len(todo_list)):
         todo = todo_list[index]
-        if todo.id == id :
+        if todo.id == id:
             todo_list.pop(index)
             return {
-                "message" : "todo 삭제 완료"
+                "message": "todo 삭제 완료!!"
             }
-        
     return {
-        "message" : "id가 존재하지 않습니다"
+        "message": "id가 존재하지 않음!!"
     }
